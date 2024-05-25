99f33b2cbcee7687619e35cdf44a36fb5dc9f56f
Milosian <milo.jazzy@hotmail.com>
2024-05-25 22:37:23 +0200

    Finished alghorithm

--- ToDo/Form1.Designer.cs
+++ ToDo/Form1.Designer.cs
@@ -29,20 +29,28 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
             this.zadanietb = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.dodajbtn = new System.Windows.Forms.Button();
             this.edytujbtn = new System.Windows.Forms.Button();
-            this.priorytetbtn = new System.Windows.Forms.Button();
             this.usunbtn = new System.Windows.Forms.Button();
             this.zadaniaTable = new System.Windows.Forms.DataGridView();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
             this.terminDate = new System.Windows.Forms.DateTimePicker();
             this.priorytetSelect = new System.Windows.Forms.ComboBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.refreshBtn = new System.Windows.Forms.Button();
+            this.idNum = new System.Windows.Forms.NumericUpDown();
+            this.idLbl = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.idEdit = new System.Windows.Forms.NumericUpDown();
+            this.updateBtn = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.zadaniaTable)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.idNum)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.idEdit)).BeginInit();
             this.SuspendLayout();
             // 
             // zadanietb
@@ -50,51 +58,47 @@
             this.zadanietb.Location = new System.Drawing.Point(12, 54);
             this.zadanietb.Multiline = true;
             this.zadanietb.Name = "zadanietb";
-            this.zadanietb.Size = new System.Drawing.Size(263, 59);
+            this.zadanietb.Size = new System.Drawing.Size(349, 59);
             this.zadanietb.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(10, 38);
+            this.label1.Font = new System.Drawing.Font("Jost", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(12, 32);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.Size = new System.Drawing.Size(85, 19);
             this.label1.TabIndex = 1;
             this.label1.Text = "Treść zadania:";
             // 
             // dodajbtn
             // 
-            this.dodajbtn.Location = new System.Drawing.Point(281, 54);
+            this.dodajbtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(192)))), ((int)(((byte)(90)))));
+            this.dodajbtn.FlatAppearance.BorderSize = 0;
+            this.dodajbtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.dodajbtn.Font = new System.Drawing.Font("Jost", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.dodajbtn.ForeColor = System.Drawing.Color.White;
+            this.dodajbtn.Location = new System.Drawing.Point(367, 54);
             this.dodajbtn.Name = "dodajbtn";
-            this.dodajbtn.Size = new System.Drawing.Size(82, 59);
+            this.dodajbtn.Size = new System.Drawing.Size(69, 59);
             this.dodajbtn.TabIndex = 2;
             this.dodajbtn.Text = "Dodaj";
-            this.dodajbtn.UseVisualStyleBackColor = true;
+            this.dodajbtn.UseVisualStyleBackColor = false;
             this.dodajbtn.Click += new System.EventHandler(this.dodajbtn_Click);
             // 
             // edytujbtn
             // 
-            this.edytujbtn.Location = new System.Drawing.Point(288, 194);
+            this.edytujbtn.Location = new System.Drawing.Point(194, 495);
             this.edytujbtn.Name = "edytujbtn";
-            this.edytujbtn.Size = new System.Drawing.Size(75, 23);
+            this.edytujbtn.Size = new System.Drawing.Size(82, 23);
             this.edytujbtn.TabIndex = 4;
             this.edytujbtn.Text = "Edytuj";
             this.edytujbtn.UseVisualStyleBackColor = true;
             this.edytujbtn.Click += new System.EventHandler(this.edytujbtn_Click);
             // 
-            // priorytetbtn
-            // 
-            this.priorytetbtn.Location = new System.Drawing.Point(288, 223);
-            this.priorytetbtn.Name = "priorytetbtn";
-            this.priorytetbtn.Size = new System.Drawing.Size(75, 23);
-            this.priorytetbtn.TabIndex = 5;
-            this.priorytetbtn.Text = "Priorytet";
-            this.priorytetbtn.UseVisualStyleBackColor = true;
-            this.priorytetbtn.Click += new System.EventHandler(this.priorytetbtn_Click);
-            // 
             // usunbtn
             // 
-            this.usunbtn.Location = new System.Drawing.Point(288, 252);
+            this.usunbtn.Location = new System.Drawing.Point(12, 494);
             this.usunbtn.Name = "usunbtn";
             this.usunbtn.Size = new System.Drawing.Size(75, 23);
             this.usunbtn.TabIndex = 6;
@@ -104,19 +108,29 @@
             // 
             // zadaniaTable
             // 
-            this.zadaniaTable.AutoGenerateColumns = false;
+            this.zadaniaTable.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.zadaniaTable.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.zadaniaTable.BackgroundColor = System.Drawing.Color.White;
             this.zadaniaTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.zadaniaTable.DataSource = this.bindingSource1;
-            this.zadaniaTable.Location = new System.Drawing.Point(12, 194);
+            this.zadaniaTable.Location = new System.Drawing.Point(12, 177);
             this.zadaniaTable.Name = "zadaniaTable";
-            this.zadaniaTable.Size = new System.Drawing.Size(265, 294);
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Jost", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.zadaniaTable.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.zadaniaTable.Size = new System.Drawing.Size(421, 309);
             this.zadaniaTable.TabIndex = 7;
             // 
             // terminDate
             // 
-            this.terminDate.Location = new System.Drawing.Point(12, 139);
+            this.terminDate.Font = new System.Drawing.Font("Jost", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.terminDate.Location = new System.Drawing.Point(12, 138);
             this.terminDate.Name = "terminDate";
-            this.terminDate.Size = new System.Drawing.Size(176, 20);
+            this.terminDate.Size = new System.Drawing.Size(248, 23);
             this.terminDate.TabIndex = 8;
             // 
             // priorytetSelect
@@ -127,49 +141,113 @@
             "średni",
             "wysoki",
             "najwyższy"});
-            this.priorytetSelect.Location = new System.Drawing.Point(194, 138);
+            this.priorytetSelect.Location = new System.Drawing.Point(266, 138);
             this.priorytetSelect.Name = "priorytetSelect";
-            this.priorytetSelect.Size = new System.Drawing.Size(82, 21);
+            this.priorytetSelect.Size = new System.Drawing.Size(94, 21);
             this.priorytetSelect.TabIndex = 9;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(9, 122);
+            this.label2.Font = new System.Drawing.Font("Jost", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(12, 116);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(96, 13);
+            this.label2.Size = new System.Drawing.Size(110, 19);
             this.label2.TabIndex = 10;
             this.label2.Text = "Termin wykonania:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(191, 122);
+            this.label3.Font = new System.Drawing.Font("Jost", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(262, 116);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.Size = new System.Drawing.Size(57, 19);
             this.label3.TabIndex = 11;
             this.label3.Text = "Priorytet:";
+            // 
+            // refreshBtn
+            // 
+            this.refreshBtn.Location = new System.Drawing.Point(367, 495);
+            this.refreshBtn.Name = "refreshBtn";
+            this.refreshBtn.Size = new System.Drawing.Size(66, 23);
+            this.refreshBtn.TabIndex = 12;
+            this.refreshBtn.Text = "Odśwież";
+            this.refreshBtn.UseVisualStyleBackColor = true;
+            this.refreshBtn.Click += new System.EventHandler(this.refreshBtn_Click);
+            // 
+            // idNum
+            // 
+            this.idNum.Location = new System.Drawing.Point(114, 497);
+            this.idNum.Name = "idNum";
+            this.idNum.Size = new System.Drawing.Size(61, 20);
+            this.idNum.TabIndex = 13;
+            // 
+            // idLbl
+            // 
+            this.idLbl.AutoSize = true;
+            this.idLbl.Location = new System.Drawing.Point(93, 500);
+            this.idLbl.Name = "idLbl";
+            this.idLbl.Size = new System.Drawing.Size(21, 13);
+            this.idLbl.TabIndex = 14;
+            this.idLbl.Text = "ID:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(278, 500);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(21, 13);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "ID:";
+            // 
+            // idEdit
+            // 
+            this.idEdit.Location = new System.Drawing.Point(299, 497);
+            this.idEdit.Name = "idEdit";
+            this.idEdit.Size = new System.Drawing.Size(61, 20);
+            this.idEdit.TabIndex = 15;
+            // 
+            // updateBtn
+            // 
+            this.updateBtn.Font = new System.Drawing.Font("Jost", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.updateBtn.Location = new System.Drawing.Point(367, 137);
+            this.updateBtn.Name = "updateBtn";
+            this.updateBtn.Size = new System.Drawing.Size(66, 23);
+            this.updateBtn.TabIndex = 17;
+            this.updateBtn.Text = "Aktualizuj";
+            this.updateBtn.UseVisualStyleBackColor = true;
+            this.updateBtn.Click += new System.EventHandler(this.updateBtn_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(378, 517);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(445, 525);
+            this.Controls.Add(this.updateBtn);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.idEdit);
+            this.Controls.Add(this.idLbl);
+            this.Controls.Add(this.idNum);
+            this.Controls.Add(this.refreshBtn);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.priorytetSelect);
             this.Controls.Add(this.terminDate);
             this.Controls.Add(this.zadaniaTable);
             this.Controls.Add(this.usunbtn);
-            this.Controls.Add(this.priorytetbtn);
             this.Controls.Add(this.edytujbtn);
             this.Controls.Add(this.dodajbtn);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.zadanietb);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Form1";
             this.Text = "ToDo";
             ((System.ComponentModel.ISupportInitialize)(this.zadaniaTable)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.idNum)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.idEdit)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -181,7 +259,6 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button dodajbtn;
         private System.Windows.Forms.Button edytujbtn;
-        private System.Windows.Forms.Button priorytetbtn;
         private System.Windows.Forms.Button usunbtn;
         private System.Windows.Forms.DataGridView zadaniaTable;
         private System.Windows.Forms.DateTimePicker terminDate;
@@ -189,6 +266,12 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.Button refreshBtn;
+        private System.Windows.Forms.NumericUpDown idNum;
+        private System.Windows.Forms.Label idLbl;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown idEdit;
+        private System.Windows.Forms.Button updateBtn;
     }
 }
 
--- ToDo/Form1.cs
+++ ToDo/Form1.cs
@@ -7,68 +7,137 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SQLite;
 using System.Data.SqlClient;
-using static System.Net.Mime.MediaTypeNames;
+//using static System.Net.Mime.MediaTypeNames;
 namespace ToDo
 {
     public partial class Form1 : Form
     {
-        public static SQLiteConnection conn;
+        
+        private SQLiteConnection conn;
+        private string filePath = @"F:\repos\ToDo\ToDo\data\lista.db";
         public Form1()
         {
             InitializeComponent();
             InitializeDatabase();
+            PokazZadania();
         }
-        public static void InitializeDatabase()
+        public void InitializeDatabase()
         {
-            string dbPath = @"C:\Users\3B GR1\source\repos\ToDo\ToDo\data\listaZadan.db";
-            conn = new SQLiteConnection($"Data Source={dbPath};Version=3;");
+            conn = new SQLiteConnection($"Data Source={filePath};Version=3;");
             conn.Open();
-
-            using (var cmd = new SQLiteCommand("CREATE TABLE IF NOT EXISTS zadania(Id INTEGER PRIMARY KEY, tresc TEXT NOT NULL, termin DATE NOT NULL, status TEXT NOT NULL)", conn))
+            
+            using (var cmd = new SQLiteCommand("CREATE TABLE IF NOT EXISTS zadania(Id INTEGER PRIMARY KEY, tresc TEXT NOT NULL, termin DATE NOT NULL, priorytet TEXT NOT NULL)", conn))
             {
                 cmd.ExecuteNonQuery();
             }
+        }
+        private void PokazZadania()
+        {
+            string sqlCmdShow = "SELECT * from zadania;";
+            using (SQLiteCommand cmd = new SQLiteCommand(sqlCmdShow, conn))
+            {
+                var reader = cmd.ExecuteReader();
+                DataTable table = new DataTable();
+                table.Load(reader);
+                zadaniaTable.DataSource = table;
+            }
+        }
+        private void RefreshList()
+        {
+            InitializeDatabase();
+            PokazZadania();
         }
 
         private void dodajbtn_Click(object sender, EventArgs e)
         {
             string trescZadania = zadanietb.Text;
-            DateTime terminWykonania;
-            terminWykonania = terminDate.Value.Date;
+            DateTime termin = terminDate.Value;
+            string formattedDate = termin.ToString("yyyy-MM-dd");
             string priorytet = priorytetSelect.Text;
 
-            using (var cmd = new SQLiteCommand("INSERT INTO zadania(tresc, termin, status) VALUES(@trescZadania, @terminWykonania, @priorytet);", conn))
+            using (var cmd = new SQLiteCommand("INSERT INTO zadania(tresc, termin, priorytet) VALUES(@trescZadania, @terminWykonania, @priorytet);", conn))
             {
                 cmd.Parameters.AddWithValue("@trescZadania", trescZadania);
-                cmd.Parameters.AddWithValue("@terminWykonania", terminWykonania);
+                cmd.Parameters.AddWithValue("@terminWykonania", formattedDate);
                 cmd.Parameters.AddWithValue("@priorytet", priorytet);
                 int rowsAffected = cmd.ExecuteNonQuery();
 
                 // Check if any rows were affected
                 if (rowsAffected > 0)
                 {
-                    MessageBox.Show("Values inserted successfully into the database!");
+                    MessageBox.Show("Zadanie zostało dodane pomyślnie!");
                 }
                 else
                 {
-                    MessageBox.Show("Failed to insert values into the database.");
+                    MessageBox.Show("BŁĄD! Nie udało się dodać zadania");
+                }
+            }
+            RefreshList();
+        }
+        private void usunbtn_Click(object sender, EventArgs e)
+        {
+            MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+            DialogResult result;
+            result = MessageBox.Show("Czy na pewno chcesz usunąć zadanie?", "Powierdzenie usunięcia", buttons);
+            if(result == DialogResult.Yes)
+            {
+                int id = Convert.ToInt32(idNum.Value);
+                string queryDelete = "DELETE FROM zadania WHERE id = @id;";
+                using (var cmd = new SQLiteCommand(queryDelete, conn))
+                {
+                    cmd.Parameters.AddWithValue("@id", id);
+                    int rowsAffected = cmd.ExecuteNonQuery();
+                }
+            }
+            RefreshList();
+        }
+        int id;
+        string tresc;
+        string priorytet;
+        string formattedDate;
+        public void edytujbtn_Click(object sender, EventArgs e)
+        {
+            id = Convert.ToInt32(idEdit.Value);
+            string query = "SELECT tresc, termin, priorytet FROM zadania WHERE id = @id";
+            using(var cmd = new SQLiteCommand(query, conn))
+            {
+                cmd.Parameters.AddWithValue("id", id);
+                
+                SQLiteDataReader reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    tresc = reader["tresc"].ToString();
+                    DateTime termin = Convert.ToDateTime(reader["termin"]);
+                    priorytet = reader["priorytet"].ToString();
+
+                    zadanietb.Text = tresc;
+                    terminDate.Value = termin;
+                    priorytetSelect.SelectedItem = priorytet;
                 }
             }
         }
-
-        private void edytujbtn_Click(object sender, EventArgs e)
+        private void updateBtn_Click(object sender, EventArgs e)
         {
+            formattedDate = terminDate.Value.ToString("yyyy-MM-dd");
+            tresc = zadanietb.Text;
+            priorytet = priorytetSelect.SelectedItem.ToString();
+            string query = "UPDATE zadania SET tresc = @tresc, termin = @termin, priorytet = @priorytet WHERE id = @id";
+            using (var cmd = new SQLiteCommand(query, conn))
+            {
+                cmd.Parameters.AddWithValue("@tresc", tresc);
+                cmd.Parameters.AddWithValue("@termin", formattedDate);
+                cmd.Parameters.AddWithValue("@priorytet", priorytet);
+                cmd.Parameters.AddWithValue("@id", id);
 
+                int rowsAffected = cmd.ExecuteNonQuery();
+            };
+
+            RefreshList();
         }
 
-        private void priorytetbtn_Click(object sender, EventArgs e)
+        private void refreshBtn_Click(object sender, EventArgs e)
         {
-
-        }
-
-        private void usunbtn_Click(object sender, EventArgs e)
-        {
-
+            RefreshList();
         }
     }
 }
